5538a0fb3e17aea36829ab2391375f1e31afe2ce
Vaibhav Narayan Gole <vaibhavgole88@gmail.com>
2023-10-24 13:46:45 +0530

    create custom middleware in index3 and in index2 use middleware morgan

--- index3.js
+++ index3.js
@@ -3,10 +3,16 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next){
+  console.log("Request Method: "+req.method);
+  console.log("Request URL: "+req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello in custome middleware");
 });
 
 app.listen(port, () => {
