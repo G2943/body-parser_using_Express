986e6f3f1262d56e7a862382bed173c2fa91b9ab
Vaibhav Narayan Gole <vaibhavgole88@gmail.com>
2023-10-24 14:47:25 +0530

    use middleware to add input text and to show

--- index4.js
+++ index4.js
@@ -1,7 +1,33 @@
-import express from "express";
+import express from "express";  //middleware
+import bodyParser from "body-parser"; //body-parser middleware
+
+//Getting full file path
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandName = "";
+
+function bandNameGenerator(req, res, next) {
+  console.log(req.body);
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bandNameGenerator);
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your bandname is:</h1><h2>${bandName}</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
